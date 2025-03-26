ab8a73517b5a92e1c2d89a3b3051f49dce3b549e
manyajain27 <jainmanya2701@gmail.com>
2025-03-26 11:21:09 +0530

    database changes and admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Worker)
+admin.site.register(Job)
+admin.site.register(Review)
+admin.site.register(Transaction)
