e7077c87e758fdc9dcb78332e0580b35a7d8de63
Advaith Manoj <advaithsince2005@gmail.com>
2025-03-27 22:41:17 +0530

    Job owner permission

--- base/permissions.py
+++ base/permissions.py
@@ -10,3 +10,12 @@ class IsCustomer(permissions.BasePermission):
     def has_permission(self, request, view):
         # Ensure user is authenticated and is a customer
         return request.user.is_authenticated and request.user.is_customer()
+    
+class IsJobOwner(permissions.BasePermission):
+    """
+    Permission to allow only the job creator (customer) to update it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Ensure the user is authenticated and is the owner of the job
+        return obj.customer.user == request.user
